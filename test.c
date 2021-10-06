e4df09d6c4073706f3c6f14b57f2c49051b6d1e1
tevoran <j.pfeiffer93@gmx.de>
2021-10-06 22:18:48 +0200

    first screenshot

--- test.c
+++ test.c
@@ -3,10 +3,10 @@
 
 int main()
 {
-	TG_init("test window", 1366, 768, false);
+	TG_init("test window", 1920, 1080, true);
 	TG_flip(0.0,0.0,0.0);
 	TG_render();
 	TG_flip(0.1,0.25,0.1);
-	SDL_Delay(3000);
+	SDL_Delay(10000);
 	return 0;
 }
